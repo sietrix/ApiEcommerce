22690608e4d4c468c6f2a3c28898414d25f1567d
Jose A. Sánchez <jsanchez.works@gmail.com>
2026-04-22 13:54:51 +0200

    Error: autenticación en Swagger

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
@@ -62,7 +63,38 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 // ------------- SwaggerGen -------------
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "Nuestra API utiliza la Autenticación JWT usando el esquema Bearer. \n\r\n\r" +
+                        "Ingresa la palabra a continuación el token generado en login.\n\r\n\r" +
+                        "Ejemplo: \"12345abcdef\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer"
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+    {
+        new OpenApiSecurityScheme
+        {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        },
+        Scheme = "oauth2",
+        Name = "Bearer",
+        In = ParameterLocation.Header
+        },
+        new List<string>()
+    }
+    });
+    }
+);
 
 // CORS
 builder.Services.AddCors(options =>
