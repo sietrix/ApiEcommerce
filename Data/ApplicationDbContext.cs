edc554e65a9baddcab5fdd017dfd438ea44be56f
Jose A. Sánchez <jsanchez.works@gmail.com>
2026-05-07 09:50:37 +0200

    Creación de modelo y soporte para uso de Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,8 +10,14 @@ public class ApplicationDbContext : DbContext
 
   }
 
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+  }
+
   public DbSet<Category> Categories { get; set; }
   public DbSet<Product> Products { get; set; }
   public DbSet<User> Users { get; set; }
+  public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
 }
