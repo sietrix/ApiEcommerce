afb7be144e5b40c39c9dbbd1d9e1a241102498c3
Jose A. Sánchez <jsanchez.works@gmail.com>
2026-05-09 13:15:43 +0200

    Subida de imagen al crear producto

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -78,6 +78,31 @@ namespace ApiEcommerce.Controllers
             }
 
             var product = _mapper.Map<Product>(createProductDto);
+            // Agregando la imagen
+            if (createProductDto.Image != null)
+            {
+                string fileName = product.ProductId + Guid.NewGuid().ToString() + Path.GetExtension(createProductDto.Image.FileName);
+                var imagesFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "ProductsImages");
+                if (!Directory.Exists(imagesFolder))
+                {
+                    Directory.CreateDirectory(imagesFolder);
+                }
+                var filePath = Path.Combine(imagesFolder, fileName);
+                FileInfo file = new FileInfo(filePath);
+                if (file.Exists)
+                {
+                    file.Delete();
+                }
+                using var fileStream = new FileStream(filePath, FileMode.Create);
+                createProductDto.Image.CopyTo(fileStream);
+                var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+                product.ImgUrl = $"{baseUrl}/ProductsImages/{fileName}";
+                product.ImgUrlLocal = filePath;
+            }
+            else
+            {
+                product.ImgUrl = "https://placehold.co/300x300";
+            }
 
             if (!_productRepository.CreateProduct(product))
             {
